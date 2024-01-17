cad7067fb3e1b2925f0850c4bca13ca133d963c5
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-17 10:49:52 -0400

    moved input functions into input.js

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-
-const {connect} = require("./client");
-// establishes a connection with the game sever
-
-const setupInput = function (callback) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-}
-setupInput(handleUserInput);
 console.log("Connecting ...");
 connect();
+
+setupInput();
